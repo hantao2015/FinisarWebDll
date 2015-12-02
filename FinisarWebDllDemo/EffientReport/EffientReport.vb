429c7a9c334f948a96d5b7257e8dc3c3edbd727d
hantao2015 <hantao@realsun.biz>
2015-12-02 12:23:52 +0800

    new

---
+++ FinisarWebDllDemo/EffientReport/EffientReport.vb
@@ -0,0 +1,20 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Net
+Imports System.Text
+Imports System.Threading
+Imports System.Threading.Tasks
+Imports System.Collections
+Imports MiniUiAppCode.Utils
+Namespace Finisar.EffientReport
+    Public Class EffientReport
+        Private m_strBaseUrl As String = "http://121.199.9.136:8082/rispweb/"
+        Private m_user As String = ""
+        Private m_upass As String = ""
+        Sub New(ByVal strBaseUrl As String, ByVal User As String, ByVal Upass As String)
+
+        End Sub
+
+    End Class
+End Namespace
+
---
+++ FinisarWebDllDemo/EffientReport/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的一般信息由以下
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+'查看程序集特性的值
+
+<Assembly: AssemblyTitle("EffientReport")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("EffientReport")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("f2b646ba-2e27-4873-9287-bfb421644526")> 
+
+' 程序集的版本信息由下列四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+'可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”: :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
